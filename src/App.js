eeb52bc245c6c8427476a9ca167349c0aa077d9f
Mike Ingber <michael.ingber@nytimes.com>
2021-06-03 14:12:36 -0400

    board wraps around

--- src/App.js
+++ src/App.js
@@ -6,6 +6,13 @@ import './App.css';
 //   pits: number[][]
 // }
 
+// 14 pits for now, assume there are always 14 pits. future adapt to different nums of pits
+// player 1, player 2
+// player1's home pit is 0
+// player1's row is 1-6
+// player2's home is 7
+// player2's row is 8-13
+
 export const manc = (board, pitIdx) => {
   const pits = board.pits.map(pit => ([...pit]))
   const pickedUp = pits[pitIdx]
@@ -17,22 +24,52 @@ export const manc = (board, pitIdx) => {
 }
 
 const initialBoard = {
-  pits: [[1,2,3,4],[5,6,7,8],[9,10,11],[12],[13,14,15,16,17,18],[19,20,21,22],[23,24]]
+  pits: [
+    [1,2,3,4],
+    [5,6,7,8],
+    [9,10,11],
+    [12],
+    [13,14,15,16,17,18],
+    [19,20,21,22],
+    [23,24],
+    [101,102,103,104],
+    [105,106,107,108],
+    [109,110,111],
+    [112],
+    [113,114,115,116,117,118],
+    [119,120,121,122],
+    [123,124]
+  ]
 }
 
 function App() {
   const [board, setBoard] = React.useState(initialBoard)
 
+  const renderPit = (index) => (
+    <ul key={index} onClick={() => setBoard(manc(board, index))}>
+      {board.pits[index].map(stoneId => (
+        <li key={stoneId}>{stoneId}</li>
+      ))}
+    </ul>
+  )
+
   return (
     <div>
       <div className="pits">
-        {board.pits.map((pit, i) => (
-          <ul key={i} onClick={() => setBoard(manc(board, i))}>
-            {pit.map(stoneId => (
-              <li key={stoneId}>{stoneId}</li>
-            ))}
-          </ul>
-        ))}
+        <div className="home">
+          {renderPit(0)}
+        </div>
+        <div className="rows">
+          <div className="top">
+            {[1,2,3,4,5,6].map(renderPit)}
+          </div>
+          <div className="bottom">
+            {[8,9,10,11,12,13].reverse().map(renderPit)}
+          </div>
+        </div>
+        <div className="home">
+          {renderPit(7)}
+        </div>
       </div>
     </div>
   );
